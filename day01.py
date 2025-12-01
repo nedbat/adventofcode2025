97b94529372cfd38de007ec66b27d7f53ca39950
Ned Batchelder <ned@nedbatchelder.com>
2025-12-01 07:02:47 -0500

    day 1, part 2 two ways

--- day01.py
+++ day01.py
@@ -13,4 +13,37 @@ for num in nums:
     if pos == 0:
         zeros += 1
 
-print(f"There were {zeros} zeros")
+print(f"Part 1: There were {zeros} zeros")
+
+# Part 2 the simplistic way.
+
+pos = 50
+zeros = 0
+for num in nums:
+    turn = -1 if num < 0 else 1
+    for _ in range(abs(num)):
+        pos += turn
+        pos %= 100
+        if pos == 0:
+            zeros += 1
+
+print(f"Part 2: there were {zeros} zeros")
+
+# Part 2 a better way.
+
+pos = 50
+zeros = 0
+for num in nums:
+    pos0 = pos
+    pos += num
+    if pos > 0:
+        zeros += pos // 100
+    else:
+        zeros += abs(pos // 100)
+        if pos % 100 == 0:
+            zeros += 1
+        if pos0 == 0:
+            zeros -= 1
+    pos %= 100
+
+print(f"Part 2: there were {zeros} zeros")
