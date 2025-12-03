403015b103b80d0e013b85e40ff20e96566bb550
Ned Batchelder <ned@nedbatchelder.com>
2025-12-03 06:03:32 -0500

    day 3, part 2

--- day03.py
+++ day03.py
@@ -15,10 +15,28 @@ def max_joltage(bank):
 def test_max_joltage():
     assert [max_joltage(b) for b in TEST_INPUT] == [98, 89, 78, 92]
 
-def part1(bank):
-    return sum(map(max_joltage, bank))
+def part1(banks):
+    return sum(map(max_joltage, banks))
 
 def test_part1():
     assert part1(TEST_INPUT) == 357
 
 print(f"Part 1: max joltage = {part1(REAL_INPUT)}")
+
+def max_joltage_n(bank, n):
+    digits = []
+    for i in range(n):
+        # Grrr: it would be much more convenient if b[:-0] went to the end.
+        end = -(n-1-i)
+        digit = max(bank[:(end if end else None)])
+        bank = bank[bank.find(digit)+1:]
+        digits.append(digit)
+    return int("".join(digits))
+
+def test_max_joltage_n():
+    assert [max_joltage_n(b, 12) for b in TEST_INPUT] == [987654321111, 811111111119, 434234234278, 888911112111]
+
+def part2(banks):
+    return sum(max_joltage_n(b, 12) for b in banks)
+
+print(f"Part 2: max_joltage = {part2(REAL_INPUT)}")
