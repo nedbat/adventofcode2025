91d1c05ae68d20357a818d775170707710379749
Ned Batchelder <ned@nedbatchelder.com>
2025-12-05 08:15:47 -0500

    day 5, part 2: doesn't work

--- day05.py
+++ day05.py
@@ -1,5 +1,7 @@
 import dataclasses
 
+import pytest
+
 TEST_INPUT = """\
 3-5
 10-14
@@ -27,7 +29,9 @@ class Db:
         db = cls([], [])
         for line in text.splitlines():
             if "-" in line:
-                db.fresh_ranges.append(tuple(map(int, line.split("-"))))
+                pair = tuple(map(int, line.split("-")))
+                assert pair[0] <= pair[1]
+                db.fresh_ranges.append(pair)
             elif line:
                 db.available.append(int(line))
         return db
@@ -55,3 +59,69 @@ def test_part1():
 
 
 print(f"Part 1: there are {part1(REAL_INPUT)} fresh ingredients")
+
+
+# Part 2: it doesn't work, and I'm not enjoying debugging it. Done.
+
+def overlap(r1, r2):
+    # https://nedbatchelder.com/blog/201310/range_overlap_in_two_compares.html
+    return r1[1] >= r2[0] and r2[1] >= r1[0]
+
+
+def split_ranges(r1, r2):
+    if overlap(r1, r2):
+        a = min(r1[0], r2[0])
+        b = max(r1[0], r2[0])
+        c = min(r1[1], r2[1])
+        d = max(r1[1], r2[1])
+        splits = set()
+        return {(a, b), (b, c), (c, d)}
+    else:
+        return {r1, r2}
+
+
+@pytest.mark.parametrize(
+    "r1, r2, res",
+    [
+        ((10, 20), (30, 40), {(10, 20), (30, 40)}),
+        ((10, 20), (15, 25), {(10, 15), (15, 20), (20, 25)}),
+        ((15, 25), (10, 20), {(10, 15), (15, 20), (20, 25)}),
+        ((10, 20), (10, 20), {(10, 10), (10, 20), (20, 20)}),
+        ((10, 10), (10, 20), {(10, 10), (10, 20)}),
+    ],
+)
+def test_split_ranges(r1, r2, res):
+    assert split_ranges(r1, r2) == res
+
+
+def union_ranges(rs):
+    print(f"starting with {rs}")
+    splits = {rs[0]}
+    for r in rs:
+        new_splits = set()
+        for s in splits:
+            new_splits.update(split_ranges(r, s))
+        print(f"intermediate: {new_splits=}")
+        splits = sorted((a, b) for a, b in new_splits if a != b)
+        print(f"intermediate: {splits=}")
+        if len(splits) > 1:
+            new_splits = set()
+            for r1, r2 in zip(splits, splits[1:]):
+                new_splits.update(split_ranges(r1, r2))
+            print(f"intermediate: {new_splits=}")
+            splits = sorted((a, b) for a, b in new_splits if a != b)
+        print(f"adding {r=}, {splits=}")
+    return splits
+
+
+def part2(text):
+    db = Db.parse(text)
+    ranges = [(a, b + 1) for a, b in db.fresh_ranges]
+    union = union_ranges(ranges)
+    print(f"done: {sorted(union)}")
+    total = sum(b - a for a, b in union)
+    return total
+
+
+def test_part2():
+    assert part2(TEST_INPUT) == 14
